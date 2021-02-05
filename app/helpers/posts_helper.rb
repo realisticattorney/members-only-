99e115796ec4b14d4c5ed0842e95dd789ac703f6
realisticattorney <g.axl.aquila@gmail.com>
2021-02-05 18:23:34 -0300

    remove loop & logic overall from views. add Post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+
+  def post_author_access(posts)
+    result = "#{}"
+    posts.each do |p|
+      result << "<tr> <td>#{p.content}</td>"
+      if user_signed_in?
+        result << "<td>#{p.user.username}</td> </tr>"
+      end
+    end
+    result.html_safe
+  end
 end
