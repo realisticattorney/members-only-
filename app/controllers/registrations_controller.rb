2fef14cd5f210e49fff9095d3ccffb7e089c5b48
realisticattorney <g.axl.aquila@gmail.com>
2021-02-04 21:08:50 -0300

    finish

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,16 +2,16 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit( :email, 
-                                  :password, 
+    params.require(:user).permit( :email,
+                                  :password,
                                   :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit( :username, 
-                                  :email, 
-                                  :password, 
-                                  :password_confirmation, 
+    params.require(:user).permit( :username,
+                                  :email,
+                                  :password,
+                                  :password_confirmation,
                                   :current_password)
   end
 end
